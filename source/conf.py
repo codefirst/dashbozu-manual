dfce1161e710c930aa00cc88261dcd1a8afe78c3
mallowlabs <mallowlabs@gmail.com>
2014-02-20 00:09:34 +0900

    Updated: copyright

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dashbozu manual'
-copyright = u'2012, codefirst.org'
+copyright = u'2012-2014, codefirst.org'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
